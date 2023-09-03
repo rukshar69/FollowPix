8aebccd6627b803e55dae52afc91271ccb6f2a6d
rukshar69 <ruksharalam7@gmail.com>
2023-09-03 19:47:31 +0600

    changed some settings.py code

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-137paa&f6_$&z_rnkns4r@4ej-uqri^v!kg8kbe11cs3zzvu)5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ruksharahmed7.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
